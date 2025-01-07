8c90edd1abe9d38e733c41e871e450e868571ab0
Ganesh rout <151042605+ganeshrout075@users.noreply.github.com>
2025-01-07 07:11:14 +0000

    string config done

---
+++ JavaScript/string.js
@@ -0,0 +1,31 @@
+const name = "ganesh"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('javas cript')
+
+
+
+console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+ const newString = gameName.substring(0,4)
+ console.log(newString);
+
+const anotherString = gameName.slice(-6, 3)
+console.log(anotherString);
+
+const newstringone = "   ganesh  "
+console.log(newstringone);
+console.log(newstringone.trim());
+
+const url = "https://ganesh.com/ganesh%20rout"
+console.log(url.replace('%20','-'));
+
+console.log
+(url.includes('rout'));
+
+console.log(gameName.slice(''));
