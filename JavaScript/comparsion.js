d4149b0753718d81975e3aa3b3e399ace43ae244
Ganesh rout <151042605+ganeshrout075@users.noreply.github.com>
2025-01-07 06:07:23 +0000

    code of stack and heap

--- JavaScript/comparsion.js
+++ JavaScript/comparsion.js
@@ -6,3 +6,32 @@ console.log(null<=0);
 console.log(undefined ==0);
 console.log(undefined > 0);
 console.log(undefined < 0);
+
+
+// stack(primitive), Heap(non-primitive)
+
+let myYoutubename ="ganeshroutdotcom"
+
+let anothername =myYoutubename
+anothername ="chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "routg212.com",
+    adress:"noida",
+    phno: ""
+}
+
+let userTwo = userOne
+
+userTwo.phno = "7676470936"
+
+console.log(userOne);
+console.log(userOne.phno);
+console.log(userTwo);
+console.log(userTwo.phno);
+
+
+
